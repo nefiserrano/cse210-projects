bd61526c971cdead806326660d78de1343cd0b2a
nefiserrano <nefi_3902@hotmail.com>
2024-03-02 01:46:33 -0600

    Variables fixed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,9 +6,9 @@ class Program
     {
         Console.WriteLine("Hello Prep1 World!");
         Console.Write("What is your first name?");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.Write("What is your last name?");
-        string last_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
