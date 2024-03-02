3521bc24be4859934e5aaa009e4c66bd7df27b51
nefiserrano <nefi_3902@hotmail.com>
2024-03-02 03:23:19 -0600

    Finished program 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number:");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int total = 0;
+        foreach (int i in numbers)
+        {
+            total += i;
+        }
+
+        float average = ((float)total) / numbers.Count;
+
+        int max = numbers[0];
+
+        foreach (int i in numbers)
+        {
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
