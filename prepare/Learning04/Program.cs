7badeedd2b9db7d7e522435221daf782cb660562
nefiserrano <nefi_3902@hotmail.com>
2024-03-19 22:46:56 -0600

    Finished Learning Activity week 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Nefi Serrano", "Multiplication");
+        string text = assignment.GetSummary();
+        Console.WriteLine(text);
+
+        MathAssingment mathAssingment = new MathAssingment("Nefi Serrano", "Fractions", "7.3", "8-19");
+        string text1 = mathAssingment.GetHomeworkList();
+        Console.WriteLine(mathAssingment.GetSummary());
+        Console.WriteLine(text1);
+
+        WritingAssingment writingAssingment = new WritingAssingment("Nefi Serrano", "European History", "The Causes of World War II");
+        string text2 = writingAssingment.GetWritingInformation();
+        Console.WriteLine(writingAssingment.GetSummary());
+        Console.WriteLine(text2);
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class WritingAssingment : Assignment
+{
+    private string _title;
+
+    public WritingAssingment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {_studentName}";
+    }
+}
