bf8cab8270496c93e692d01b06ab8e8e6131a87d
nefiserrano <nefi_3902@hotmail.com>
2024-03-26 22:05:54 -0600

    Finished Learning Activity Week 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 10);
+
+        Rectangle rectangle = new Rectangle("red", 5, 20);
+
+        Circle circle = new Circle("green", 5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine(color);
+            Console.WriteLine(area);
+        }
     }
 }
